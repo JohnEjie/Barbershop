1a8d746c5202c1b53fc40cca706e4576b147ea27
JohnEjie <johnejie.ranchez@gmail.com>
2025-11-12 00:55:40 +0800

    Add homepage route for Render deployment

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -1,29 +1,15 @@
-"""
-URL configuration for barbershop project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
+# simple homepage view
 def home(request):
-    return HttpResponse("Welcome to the Barbershop!")
+    return HttpResponse("<h1>Welcome to the Barbershop API</h1><p>Server is running!</p>")
 
 urlpatterns = [
+    path('', home),  # 👈 Add this line for homepage
     path('admin/', admin.site.urls),
-    path('api/auth/', include('users.urls')),  # Changed from 'api/users/' to 'api/auth/'
+    path('api/auth/', include('users.urls')),
     path('api/barbers/', include('barbers.urls')),
     path('api/', include('appointments.api.urls')),
 ]
