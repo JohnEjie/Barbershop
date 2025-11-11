1961706b99233274fa93d707a5206d936804952e
JohnEjie <johnejie.ranchez@gmail.com>
2025-11-12 00:53:18 +0800

    Add homepage route

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the Barbershop!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
